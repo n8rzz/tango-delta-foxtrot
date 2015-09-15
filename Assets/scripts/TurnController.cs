0f0517d7856d7e24fb2b40973552a93c3c80354f
Nate Geslin <teamtomkins23@gmail.com>
2015-09-14 22:30:14 -0500

    Adds logic to increase elapsedTrunTimer after a certain number of turns * Refactors elapsedTurnTimer to count down from an int value * Moves elapsedTurnTimer back to GameManager class * Creates barebones GameBoardManager

--- Assets/scripts/TurnController.cs
+++ Assets/scripts/TurnController.cs
@@ -5,37 +5,21 @@ using System.Collections;
 public class TurnController : MonoBehaviour 
 {	
 	public GameObject currentPlayerText;
-	public GameObject elapsedTurnTimeText;
 
 	private int currentPlayer;
-	private float elapsedTurnTime;
 
 	void Awake()
 	{
         currentPlayerText = GameObject.FindGameObjectWithTag("currentTurn").gameObject;
-		elapsedTurnTimeText = GameObject.FindGameObjectWithTag("elapsedTurnTime").gameObject;
 	}
 
 	void Start()
 	{
 		currentPlayer = 0;
-		elapsedTurnTime = 0f;
+
 		_setCurrentPlayerText();
 	}
-
-	void Update()
-	{
-		elapsedTurnTime += Time.deltaTime;
-	}
-
-
-	void FixedUpdate()
-	{
-		string elapsedTurnTimeString = transformTime(elapsedTurnTime);
-
-		elapsedTurnTimeText.GetComponent<Text>().text = "Turn Time: " + elapsedTurnTimeString;
-
-	}
+	
 
 	public int getCurrentPlayer()
 	{
@@ -54,32 +38,15 @@ public class TurnController : MonoBehaviour
 			currentPlayer = 0;
 		}
 
-		print(currentPlayer);
-		_resetElapsedTrunTime();
 		_setCurrentPlayerText();
 
 		return currentPlayer;
 	}
-
-	void _resetElapsedTrunTime()
-	{
-		elapsedTurnTime = 0f;
-	}
+	
 
 	void _setCurrentPlayerText()
 	{
-		Debug.Log (currentPlayerText);
-		print("set " + currentPlayer);
 		currentPlayerText.GetComponent<Text>().text = "Current Player: " + (currentPlayer + 1).ToString();
 	}
-
-	// todo: duplicated method.  this needs to be moved to a utility class
-	string transformTime(float time) 
-	{
-		int minutes = Mathf.FloorToInt(time / 60F);
-		int seconds = Mathf.FloorToInt(time - minutes * 60);
-		string transformedTime = string.Format("{0:0}:{1:00}", minutes, seconds);
-		
-		return transformedTime;
-	}
+	
 }
