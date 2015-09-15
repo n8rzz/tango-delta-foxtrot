9eceefc7124822881cf769411fabb57cc788449c
Nate Geslin <teamtomkins23@gmail.com>
2015-09-14 19:42:00 -0500

    Adds in Alex Hofer's fixes for currentPlayerText and refactoring of Text and Script objects to gameObjects * Adds elapsedTurnTimeText that resets at the start of each turn

--- Assets/scripts/TurnController.cs
+++ Assets/scripts/TurnController.cs
@@ -5,21 +5,37 @@ using System.Collections;
 public class TurnController : MonoBehaviour 
 {	
 	public GameObject currentPlayerText;
+	public GameObject elapsedTurnTimeText;
 
 	private int currentPlayer;
+	private float elapsedTurnTime;
 
 	void Awake()
 	{
-		Debug.Log("turnController " + GetInstanceID());
         currentPlayerText = GameObject.FindGameObjectWithTag("currentTurn").gameObject;
+		elapsedTurnTimeText = GameObject.FindGameObjectWithTag("elapsedTurnTime").gameObject;
 	}
 
 	void Start()
 	{
 		currentPlayer = 0;
+		elapsedTurnTime = 0f;
 		_setCurrentPlayerText();
 	}
 
+	void Update()
+	{
+		elapsedTurnTime += Time.deltaTime;
+	}
+
+
+	void FixedUpdate()
+	{
+		string elapsedTurnTimeString = transformTime(elapsedTurnTime);
+
+		elapsedTurnTimeText.GetComponent<Text>().text = "Turn Time: " + elapsedTurnTimeString;
+
+	}
 
 	public int getCurrentPlayer()
 	{
@@ -39,16 +55,31 @@ public class TurnController : MonoBehaviour
 		}
 
 		print(currentPlayer);
+		_resetElapsedTrunTime();
 		_setCurrentPlayerText();
 
 		return currentPlayer;
 	}
 
-	//
+	void _resetElapsedTrunTime()
+	{
+		elapsedTurnTime = 0f;
+	}
+
 	void _setCurrentPlayerText()
 	{
 		Debug.Log (currentPlayerText);
 		print("set " + currentPlayer);
 		currentPlayerText.GetComponent<Text>().text = "Current Player: " + (currentPlayer + 1).ToString();
+	}
+
+	// todo: duplicated method.  this needs to be moved to a utility class
+	string transformTime(float time) 
+	{
+		int minutes = Mathf.FloorToInt(time / 60F);
+		int seconds = Mathf.FloorToInt(time - minutes * 60);
+		string transformedTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+		
+		return transformedTime;
 	}
 }
