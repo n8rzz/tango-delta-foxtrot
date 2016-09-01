257a17bd2c55edc8115f286be4b0a461d4eff6cf
Nate Geslin <teamtomkins23@gmail.com>
2016-08-31 20:41:27 -0500

    Updates FormationsCollection constructor and list -> add methods

--- Assets/scripts/FormationsCollection.cs
+++ Assets/scripts/FormationsCollection.cs
@@ -2,28 +2,26 @@
 
 public class FormationsCollection
 {
-    public List<FormationModel> formations = new List<FormationModel>(FormationModelBuilder.buildFormationModelsFromPossibleFormations());
+    public List<FormationModel> formations = new List<FormationModel>();
 
 
     public FormationsCollection()
     {
-        List<FormationModel> possibleFormations = new List<FormationModel>();
-        // possibleFormations.AddRange(FormationModelBuilder.buildFormationModelsFromPossibleFormations());
+        List<FormationModel> possibleFormations = new List<FormationModel>(FormationModelBuilder.buildFormationModelsFromPossibleFormations());
 
-        // addFormationListToCollection(possibleFormations);
+        addFormationListToCollection(possibleFormations);
     }
 
-    // void addFormationListToCollection(List<FormationModel> formationList)
-    // {
-    //     for (var i = 0; i < formationList.Count; i++)
-    //     {
-    //         addFormationToCollection(formationList[i]);
-    //     }
-    // }
+    void addFormationListToCollection(List<FormationModel> formationList)
+    {
+        for (var i = 0; i < formationList.Count; i++)
+        {
+            addFormationToCollection(formationList[i]);
+        }
+    }
 
-    // void addFormationToCollection(FormationModel formationToAdd)
-    // {
-
-    //     formations.Add(formationToAdd);
-    // }
+    void addFormationToCollection(FormationModel formationToAdd)
+    {
+        this.formations.Add(formationToAdd);
+    }
 }
