e16b821523c1e169ecac925f9b80a04503efe1b0
Nate Geslin <teamtomkins23@gmail.com>
2016-08-31 22:32:50 -0500

    Makes FormationPointModel porps public. Adds FormationModelComparator class.

---
+++ Assets/scripts/FormationModelComparator.cs
@@ -0,0 +1,9 @@
+﻿public static class FormationModelComparator
+{
+	public static bool isEqual(FormationPointModel point, FormationPointModel comparePoint)
+	{
+		return point.level == comparePoint.level && 
+			point.row == comparePoint.row && 
+			point.column == comparePoint.column;
+	}
+}
--- Assets/scripts/FormationPointModel.cs
+++ Assets/scripts/FormationPointModel.cs
@@ -1,8 +1,8 @@
 ﻿public class FormationPointModel
 {
-    int level;
-    int row;
-    int column;
+    public int level;
+    public int row;
+    public int column;
 
     public FormationPointModel(int level, int row, int column)
     {
@@ -10,19 +10,4 @@
         this.row = row;
         this.column = column;
     }
-
-    // FIXME: Move method to static utility class
-    // public bool isEqualToComparePoint(FormationPointModel comparePoint)
-    // {
-    //     return comparePoint.level == this.level &&
-    //         comparePoint.row == this.row &&
-    //         comparePoint.column == this.column;
-    // }
-
-    // public int[] toArray()
-    // {
-    //     int[] arr = new int[] {level, row, column};
-
-    //     return arr;
-    // }
 }
