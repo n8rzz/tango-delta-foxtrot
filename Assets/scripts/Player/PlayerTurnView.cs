7de3d9eab4b465b6f6ad0e5a81b95b463fe4cb1a
Nate Geslin <teamtomkins23@gamil.com>
2016-09-04 16:43:48 -0500

    Adds documentaion to PlayerTurn scripts.

--- Assets/scripts/Player/PlayerTurnView.cs
+++ Assets/scripts/Player/PlayerTurnView.cs
@@ -22,18 +22,20 @@ public class PlayerTurnView : MonoBehaviour
 	}
 	
 
-	// 
+	// updates the currentTurn text string
 	public void updateActivePlayerText(int currentPlayer)
 	{
 		setCurrentPlayerText(currentPlayer);
 	}
 
+	// sets a text string and text color based on the currenPlayer 
 	private void setCurrentPlayerText(int currentPlayer)
 	{
 		currentPlayerText.GetComponent<Text>().text = "Current Player: " + (currentPlayer + 1).ToString();
 		currentPlayerText.GetComponent<Text>().color = calculateCurrentPlayerColor(currentPlayer);
 	}
 
+	// returns a color based on the currentPlayer. color corresponds to the currentPlayer piece color.
 	private Color calculateCurrentPlayerColor(int currentPlayer)
 	{
 		return (currentPlayer == 0) ? Color.magenta : Color.blue;
